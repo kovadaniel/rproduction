596337ac1df428de80fbff2b29b1c278c3bdf2f5
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-09-29 15:57:12 +0300

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { Counter } from "./components/Counter";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import "./index.scss"
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+
 const App = () => {
   return (
     <div id="app">
-      Lorem ipsum dolor sit amet.
-      <Counter />
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
- 
+
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import { someFunction } from "./test";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 someFunction(30);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
