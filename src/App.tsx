e5631fcffb0576c41d7d22d8b23253504b32228f
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-10-08 16:46:34 +0300

    feat: custom classNames function

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 import "./styles/index.scss"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense, useContext, useState } from "react";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {hovered: true, selected: false}, [theme])}>
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
