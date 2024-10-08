eb2171c3e4a33454217fc1390fb0e6d8dce09b23
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-10-08 16:12:18 +0300

    feat: themes and styles folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import "./index.scss"
+import "./styles/index.scss"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div id="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
       <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { someFunction } from "./test";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 someFunction(30);
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
