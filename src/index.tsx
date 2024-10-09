98163f4e79892550bb5f0a66cdee560113f35ced
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-10-09 17:26:59 +0300

    build: absolute path & FSD architecture

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { someFunction } from "./test";
-import App from "./App";
+import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
-import ThemeProvider from "./theme/ThemeProvider";
+import { ThemeProvider } from "app/providers/ThemeProvider";
+
 
 someFunction(30);
 
