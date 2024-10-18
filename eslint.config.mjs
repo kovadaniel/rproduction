cdc7a236eb5cc32119845bbbbf61f6500847d2f8
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-10-18 19:20:11 +0300

    config: eslint plunin i18next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import i18next from 'eslint-plugin-i18next';
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -10,6 +10,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  i18next.configs['flat/recommended'],
   {
     rules: {
       "no-unused-vars": "off",
