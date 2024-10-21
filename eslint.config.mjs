2ce399544eb66fa33bb21c0f03ef7674b7ccc3d7
Daniil Kovalenko <danielkovalenko50@gmail.com>
2024-10-21 18:35:11 +0300

    config: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import i18next from 'eslint-plugin-i18next';
+import i18next from "eslint-plugin-i18next";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -10,8 +11,11 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  i18next.configs['flat/recommended'],
+  i18next.configs["flat/recommended"],
   {
+    plugins: {
+      "@stylistic/js": stylisticJs,
+    },
     rules: {
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "warn",
@@ -19,6 +23,11 @@ export default [
       "react/react-in-jsx-scope": "off",
       "@typescript-eslint/ban-ts-comment": "off",
       "indent": ["error", 2],
+      "no-console": "warn",
+      "@stylistic/js/quotes": ["warn", "double"],
+      "@stylistic/js/no-multiple-empty-lines": ["warn", { "max": 1 }],
+      "@stylistic/js/eol-last": ["error", "always"],
+      "@stylistic/js/no-trailing-spaces": "error"
     }
   }
 ];
